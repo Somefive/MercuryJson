34ad463285fcf3c939e18b28f589704978421c8f
Zecong Hu <huzecong@gmail.com>
2019-04-15 17:35:41 -0400

    Compatible with macOS

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,14 +1,16 @@
+#include <stdexcept>
+
 #include <cstdio>
 #include <cstdlib>
-#include <stdexcept>
+
 #include "utils.h"
 
-char * read_file(const char * filename, size_t & size) {
-    std::FILE * pfile = std::fopen(filename, "rb");
+char *read_file(const char *filename, size_t &size) {
+    std::FILE *pfile = std::fopen(filename, "rb");
     if (pfile == NULL) throw std::runtime_error("file open error");
     std::fseek(pfile, 0, SEEK_END);
     size = std::ftell(pfile);
-    char * buffer = (char *) aligned_alloc(ALIGNMENT_SIZE, size);
+    char *buffer = (char *)aligned_malloc(ALIGNMENT_SIZE, size);
     if (buffer == NULL) {
         std::fclose(pfile);
         throw std::runtime_error("allocate memory failed");
--- src/utils.h
+++ src/utils.h
@@ -1,3 +1,28 @@
-#define ALIGNMENT_SIZE 64
+#ifndef MERCURYJSON_UTILS_H
+#define MERCURYJSON_UTILS_H
 
-char * read_file(const char * filename, size_t & size);
+#include <cstdio>
+#include <cstdlib>
+
+static const size_t ALIGNMENT_SIZE = 64;
+
+char *read_file(const char *filename, size_t &size);
+
+static inline void *aligned_malloc(size_t alignment, size_t size) {
+    void *p;
+    if (posix_memalign(&p, alignment, size) != 0) { return nullptr; }
+    return p;
+}
+
+template<typename T>
+static inline void aligned_free(T *memblock) {
+    if (memblock == nullptr) { return; }
+    free(reinterpret_cast<void *>(memblock));
+}
+
+template<typename T>
+static inline void aligned_free(const T *memblock) {
+    aligned_free<T>(const_cast<T *>(memblock));
+}
+
+#endif // MERCURYJSON_UTILS_H
