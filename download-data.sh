ba947c7c0120109fdb3fae1c093eccfb9f42a329
Somefive <Somefive@foxmail.com>
2019-05-06 15:58:06 -0400

    add download-data.sh

---
+++ download-data.sh
@@ -0,0 +1,5 @@
+wget -O citylots.json https://github.com/zemirco/sf-city-lots-json/raw/master/citylots.json
+wget -O crime.json https://data.lacity.org/api/views/y8tr-7khq/rows.json
+wget -O traffic.json https://data.montgomerycountymd.gov/api/views/4mse-ku6q/rows.json
+wget -O lottery.json https://data.ny.gov/api/views/5xaw-6ayf/rows.json
+wget -O motor.json https://data.ny.gov/api/views/xe9x-a24f/rows.json
